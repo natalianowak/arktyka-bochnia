c0839b3a9826e92c5a21657d25fb9b4fb4430dcf
natalia.nowak <natalia.wioletta.nowak@gmail.com>
2020-01-31 15:10:03 +0100

    Updates

---
+++ precache-manifest.6b0901a9ea5ca3b9dd8cd3566bb12b61.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7205c4d7b82cd384fdf7dd8586c2fe41",
+    "url": "/arktyka-bochnia/index.html"
+  },
+  {
+    "revision": "956b633873017577d4d4",
+    "url": "/arktyka-bochnia/static/css/main.13aa0e5b.chunk.css"
+  },
+  {
+    "revision": "e0c1ec36c420e4d021d2",
+    "url": "/arktyka-bochnia/static/js/2.86bc10b9.chunk.js"
+  },
+  {
+    "revision": "fb76c9f8f8182087f9c180bc5fcbff28",
+    "url": "/arktyka-bochnia/static/js/2.86bc10b9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "956b633873017577d4d4",
+    "url": "/arktyka-bochnia/static/js/main.02f6e0db.chunk.js"
+  },
+  {
+    "revision": "d551b51a04d292cea722",
+    "url": "/arktyka-bochnia/static/js/runtime-main.5ba665a7.js"
+  },
+  {
+    "revision": "005645254f56f9c026fae4e82854b171",
+    "url": "/arktyka-bochnia/static/media/Exo-Black.00564525.ttf"
+  },
+  {
+    "revision": "fd0fb60790e2caebbd3964974293fbf8",
+    "url": "/arktyka-bochnia/static/media/Exo-Bold.fd0fb607.ttf"
+  },
+  {
+    "revision": "300557d530b5b680be682bb0f5611183",
+    "url": "/arktyka-bochnia/static/media/Exo-Light.300557d5.ttf"
+  },
+  {
+    "revision": "8bee4f80d69101a696aea9b436835337",
+    "url": "/arktyka-bochnia/static/media/Exo-Regular.8bee4f80.ttf"
+  },
+  {
+    "revision": "fb3d81ccab435985006fe43505b9db5c",
+    "url": "/arktyka-bochnia/static/media/lodowiec.fb3d81cc.png"
+  },
+  {
+    "revision": "101281b3b43d8b48936794076f2779c3",
+    "url": "/arktyka-bochnia/static/media/menu-tlo.101281b3.png"
+  },
+  {
+    "revision": "4e7b6d4d7e64feb35acb47ffdcaa17cc",
+    "url": "/arktyka-bochnia/static/media/sniezynki.4e7b6d4d.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/arktyka-bochnia/precache-manifest.6b0901a9ea5ca3b9dd8cd3566bb12b61.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/arktyka-bochnia/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
